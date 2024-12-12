12d1f9c79091ffc939fed3530e6d331ce709dc98
Sridhar Alagiri Samy <sridhar.alagirisamy@syncfusion.com>
2024-12-12 13:10:22 +0530

    926985: Open and save service URL updated

--- src/app/app.js
+++ src/app/app.js
@@ -3,50 +3,50 @@ define(["require", "exports", "@syncfusion/ej2-spreadsheet", "./conditional-form
     Object.defineProperty(exports, "__esModule", { value: true });
     var spreadsheet = new ej2_spreadsheet_1.Spreadsheet({
         sheets: [{
-                rows: [{
-                        height: 31,
-                        cells: [{
-                                index: 1,
-                                value: 'Inventory List',
-                            }]
-                    }],
-                ranges: [{
-                        dataSource: conditional_formatting_data_1.conditionalFormatData(),
-                        startCell: 'A2'
-                    },
-                ],
-                name: 'Inventory List',
-                conditionalFormats: [
-                    { type: 'GYRColorScale', range: 'C3:C18' },
-                    { type: 'LessThan', cFColor: 'RedFT', value: '8/30/2019', range: 'G3:G18' }
-                ],
-                columns: [{
-                        width: 100
-                    },
-                    {
-                        width: 158
-                    },
-                    {
-                        width: 72
-                    },
-                    {
-                        width: 113
-                    },
-                    {
-                        width: 113
-                    },
-                    {
-                        width: 77
-                    },
-                    {
-                        width: 97
-                    },
-                    {
-                        width: 73
-                    }]
-            },],
-        openUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open',
-        saveUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save',
+            rows: [{
+                height: 31,
+                cells: [{
+                    index: 1,
+                    value: 'Inventory List',
+                }]
+            }],
+            ranges: [{
+                dataSource: conditional_formatting_data_1.conditionalFormatData(),
+                startCell: 'A2'
+            },
+            ],
+            name: 'Inventory List',
+            conditionalFormats: [
+                { type: 'GYRColorScale', range: 'C3:C18' },
+                { type: 'LessThan', cFColor: 'RedFT', value: '8/30/2019', range: 'G3:G18' }
+            ],
+            columns: [{
+                width: 100
+            },
+            {
+                width: 158
+            },
+            {
+                width: 72
+            },
+            {
+                width: 113
+            },
+            {
+                width: 113
+            },
+            {
+                width: 77
+            },
+            {
+                width: 97
+            },
+            {
+                width: 73
+            }]
+        }],
+        openUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/open',
+        saveUrl: 'https://services.syncfusion.com/js/production/api/spreadsheet/save',
         created: function () {
             spreadsheet.merge('A1:H1');
             spreadsheet.cellFormat({ fontWeight: 'bold', textAlign: 'center' }, 'A2:H2');
